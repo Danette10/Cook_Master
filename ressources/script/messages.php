6bffa4a6bac7a6013bce4119b37a34c5bcc5277e
Dan Sebag <dan.sebag1007@gmail.com>
2023-03-30 16:25:08 +0200

    fix message success and error

--- ressources/script/messages.php
+++ ressources/script/messages.php
@@ -1,11 +1,13 @@
 <?php
-$type = isset($_GET['type']) ?? htmlspecialchars($_GET['type']);
-$message = isset($_GET['message']) ?? htmlspecialchars($_GET['message']);
+$type = isset($_GET['type']) ? htmlspecialchars($_GET['type']) : '';
+$message = isset($_GET['message']) ? htmlspecialchars($_GET['message']) : '';
 
-if($type == 'success') {
-    echo '<div class="alert alert-success" role="alert">' . $message . '</div>';
-    exit();
-} else if($type == 'error') {
-    echo '<div class="alert alert-danger" role="alert">' . $message . '</div>';
-    exit();
+if($type != '' && $message != ''){
+    if($type == 'success') {
+        echo '<div class="alert alert-success" role="alert">' . $message . '</div>';
+        exit();
+    } else if($type == 'error') {
+        echo '<div class="alert alert-danger" role="alert">' . $message . '</div>';
+        exit();
+    }
 }
