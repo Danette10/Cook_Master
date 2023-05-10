6e02e0eb83aee0a468dcca171070e39a26316082
Makadji Mamadou <makadjimamadou01@gmail.com>
2023-05-10 20:04:59 +0200

    fix bug db to add step and ingredients

--- ressources/script/form/recipeForm.php
+++ ressources/script/form/recipeForm.php
@@ -1,26 +1,21 @@
-<?php 
-session_start();
-include "../init.php";
+<?php
 include PATH_SCRIPT . "functions.php";
 
 global $db;
 
 if(isset($_POST['title']) && isset($_FILES['recipeImage']) && isset($_POST['recipeDescription']) && isset($_POST['recipeIngredients']) && isset($_POST['recipeSteps'])) {
-    $recipeTitle = $_POST['title'];
+    $recipeTitle = htmlspecialchars(ucwords(strtolower($_POST['title'])));
     $recipeImage = $_FILES['recipeImage'];
-    $recipeDescription = $_POST['recipeDescription'];
+    $recipeDescription = htmlspecialchars(ucwords(strtolower($_POST['recipeDescription'])));
     $recipeIngredients = $_POST['recipeIngredients'];
     $recipeSteps = $_POST['recipeSteps'];
-
-    htmlspecialchars(ucwords(strtolower($recipeTitle)));
-    htmlspecialchars(ucwords(strtolower($recipeDescription)));
 
     $errors = [];
 
     if(strlen($recipeTitle) < 5 || strlen($recipeTitle) > 100) {
         $errors[] = "Le titre doit contenir entre 5 et 100 caractères";
     }
-    
+
 
     if(strlen($recipeDescription) < 5 || strlen($recipeDescription) > 1000) {
         $errors[] = "La description doit contenir entre 5 et 1000 caractères";
@@ -36,11 +31,10 @@ if(isset($_POST['title']) && isset($_FILES['recipeImage']) && isset($_POST['reci
 
     $creator = $_SESSION['id'];
 
-
     if(count($errors) == 0) {
 
         $file = "";
-        
+
         $file = uploadRecipePicture($recipeImage);
 
         if(is_int($file)){
@@ -57,7 +51,7 @@ if(isset($_POST['title']) && isset($_FILES['recipeImage']) && isset($_POST['reci
 
         $date = date('Y-m-d H:i:s');
 
-        $addRecipe = $db->prepare("INSERT INTO USERS_RECIPE (recipeName, description, recipeImage, idUser, creationDate) VALUES (:recipeName, :description, :recipeImage, :creator, :creationDate)");
+        $addRecipe = $db->prepare("INSERT INTO RECIPE (recipeName, description, recipeImage, idUser, creationDate) VALUES (:recipeName, :description, :recipeImage, :creator, :creationDate)");
         $addRecipe->execute([
             'recipeName' => $recipeTitle,
             'description' => $recipeDescription,
@@ -66,43 +60,34 @@ if(isset($_POST['title']) && isset($_FILES['recipeImage']) && isset($_POST['reci
             'creationDate' => $date
         ]);
 
-        $getRecipeId = $db->prepare("SELECT idRecipe FROM USERS_RECIPE WHERE recipeName = :recipeName AND idUser = :creator AND creationDate = :creationDate");
-        $getRecipeId->execute([
-            'recipeName' => $recipeTitle,
-            'creator' => $creator,
-            'creationDate' => $date
-        ]);
+        $recipeId = $db->lastInsertId();
 
-        $recipeId = $getRecipeId->fetch();
-        $recipeId = $recipeId['idRecipe'];
-        
-    
-        for($i = 1; $i < $recipeIngredients; $i++) {
-            if(!isset($_POST['ingredientName' . $i])) {
-                $errors[] = "Veuillez remplir tout les champs ingrédients.\nVeuillez remplir le champ nom de l'ingrédient " . $i;
-            }else {
+        for ($i = 1; $i <= $recipeIngredients; $i++) { // Change "<" to "<="
+            if (!isset($_POST['ingredientName' . $i])) {
+                $errors[] = "Veuillez remplir tous les champs ingrédients.\nVeuillez remplir le champ nom de l'ingrédient " . $i;
+            } else {
                 $ingredientName = $_POST['ingredientName' . $i];
-                if(strlen($ingredientName) < 2 || strlen($ingredientName) > 100) {
+                if (strlen($ingredientName) < 2 || strlen($ingredientName) > 100) {
                     $errors[] = "Le nom de l'ingrédient " . $i . " doit contenir entre 2 et 100 caractères";
                 }
-    
-                if(!isset($_POST['ingredientQuantity' . $i])) {
+
+                if (!isset($_POST['ingredientQuantity' . $i])) {
                     $errors[] = "Veuillez remplir le champ quantité de l'ingrédient " . $i;
-                }else {
+                } else {
                     $ingredientQuantity = $_POST['ingredientQuantity' . $i];
                 }
-    
-                if(!isset($_POST['ingredientUnit' . $i])) {
+
+                if (!isset($_POST['ingredientUnit' . $i])) {
                     $errors[] = "Veuillez remplir le champ unité de l'ingrédient " . $i;
-                }else {
+                } else {
                     $ingredientUnit = $_POST['ingredientUnit' . $i];
                 }
-                
+
                 if (count($errors) > 0) {
                     $_SESSION['errors'] = $errors;
                     header("Location: " . ADDRESS_SITE . 'recettes');
                     exit();
-                }else {
+                } else {
                     $addIngredient = $db->prepare("INSERT INTO RECIPE_INGREDIENTS (ingredientName, ingredientQuantity, unit, idRecipe) VALUES (:ingredientName, :ingredientQuantity, :unit, :idRecipe)");
                     $addIngredient->execute([
                         'ingredientName' => $ingredientName,
@@ -112,39 +97,24 @@ if(isset($_POST['title']) && isset($_FILES['recipeImage']) && isset($_POST['reci
                     ]);
                 }
             }
-                
         }
-    
-        for($i = 1; $i < $recipeSteps; $i++) {
-            if(!isset($_POST['step' . $i])) {
-                $errors[] = "Veuillez remplir tout les champs étapes.\nVeuillez remplir le champ étape " . $i;
-            }else {
+        for ($i = 1; $i <= $recipeSteps; $i++) { // Change "<" to "<="
+            if (!isset($_POST['step' . $i])) {
+                $errors[] = "Veuillez remplir tous les champs étapes.\nVeuillez remplir le champ étape " . $i;
+            } else {
                 $step = $_POST['step' . $i];
-                if(strlen($step) < 2 || strlen($step) > 1000) {
+                if (strlen($step) < 2 || strlen($step) > 1000) {
                     $errors[] = "L'étape " . $i . " doit contenir entre 2 et 1000 caractères";
-                }
-
-                if(isset($_FILES['stepImage' . $i])) {
-                    $stepImage = $_FILES['stepImage' . $i];
-                    $file = uploadRecipePicture($stepImage);
-                    if(is_int($file)){
-                        $errors[] = "Erreur lors de l'upload de l'image de l'étape " . $i;
-                    }else{
-                        $stepImage = $file;
-                    }
-                }else {
-                    $stepImage = "";
                 }
 
                 if (count($errors) > 0) {
                     $_SESSION['errors'] = $errors;
                     header("Location: " . ADDRESS_SITE . 'recettes');
                     exit();
-                }else {
-                    $addStep = $db->prepare("INSERT INTO RECIPE_STEPS (stepDescription, stepImg, idRecipe) VALUES (:step, :stepImage, :idRecipe)");
+                } else {
+                    $addStep = $db->prepare("INSERT INTO RECIPE_STEPS (stepDescription, idRecipe) VALUES (:step, :idRecipe)");
                     $addStep->execute([
                         'step' => $step,
-                        'stepImage' => $stepImage,
                         'idRecipe' => $recipeId
                     ]);
                 }
@@ -153,11 +123,13 @@ if(isset($_POST['title']) && isset($_FILES['recipeImage']) && isset($_POST['reci
 
         header("Location: " . ADDRESS_SITE . 'recettes?type=success&message=La recette a bien été ajoutée');
         exit();
+    }else{
+        $_SESSION['errors'] = $errors;
+        header("Location: " . ADDRESS_SITE . 'recettes');
+        exit();
     }
-
+}else{
+    header("Location: " . ADDRESS_SITE . 'recettes');
+    exit();
 }
 
-
-
-
-?>
