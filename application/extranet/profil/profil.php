345b66355de640c1a033e14609ae1e0404d527f4
Dan Sebag <dsebag@asptt.com>
2023-05-27 18:44:59 +0200

    fix bug profil page

--- application/extranet/profil/profil.php
+++ application/extranet/profil/profil.php
@@ -61,6 +61,8 @@ if($countSub['countSub'] > 0){
 
         <main>
 
+            <h2 class="mt-3 text-center">Bienvenue sur votre profil <span style="color: #FF9B90;"><strong><?= $firstname ?></strong></span> !</h2>
+
             <div class="allInfoProfil">
 
                 <div class="imageProfil me-4">
@@ -95,25 +97,31 @@ if($countSub['countSub'] > 0){
                         <p><strong>Compte préstataire : </strong><em>En cours de validation</em></p>
                     <?php }else if($role == 4){ ?>
                         <p><strong>Compte préstataire : </strong>Validé</p>
+                    <?php }else if($role == 5){ ?>
+                        <p style="color: #FF9B90;"><strong>Administrateur</strong></p>
                     <?php } ?>
                     <?php
-                    if($subscribed['subscriptionStatus'] == 'active'){
+                    if($subscribed !== null && $subscribed['subscriptionStatus'] == 'active'){
                     ?>
                         <button class="manageSubLink btn">
                             <a href="<?= ADDRESS_SITE ?>profil/manage/subscription" class="nav-link">Gérer votre abonnement</a>
                         </button>
                     <?php
-                    }else{
+                    }else if($role != 5){
                     ?>
                         <button class="manageSubLink btn">
                             <a href="<?= ADDRESS_SITE ?>subscribe" class="nav-link">S'abonner</a>
                         </button>
                     <?php
                     }
+                    if($role != 5){
                     ?>
                     <button class="manageInvoice btn">
                         <a href="<?= ADDRESS_SITE ?>profil/manage/invoice" class="nav-link">Voir mes factures</a>
                     </button>
+                    <?php
+                    }
+                    ?>
                 </div>
 
             </div>
