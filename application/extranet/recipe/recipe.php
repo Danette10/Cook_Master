3cfee9fd6208d8516c48d2705a2970e9071359a4
AriesKira <linharesrodrigues.gil@gmail.com>
2023-06-27 18:24:10 +0200

    Added:
    Author of recipe infos

--- application/extranet/recipe/recipe.php
+++ application/extranet/recipe/recipe.php
@@ -20,6 +20,12 @@ $getRecipeSteps->execute([
 ]);
 $recipeSteps = $getRecipeSteps->fetchAll(PDO::FETCH_ASSOC);
 
+$authorInfos = $db->prepare("SELECT * FROM users WHERE idUser = :idUser");
+$authorInfos->execute([
+    'idUser' => $recipe['idUser']
+]);
+$authorInfos = $authorInfos->fetch(PDO::FETCH_ASSOC);
+$authorProfilePicture = $authorInfos['profilePicture'];
 ?>
 <body>
     <main>
@@ -27,12 +33,22 @@ $recipeSteps = $getRecipeSteps->fetchAll(PDO::FETCH_ASSOC);
             <div class="col-4"></div>
             <div class="col-4">
                 <h1 class="text-center mt-4"><?= $recipe['recipeName'] ?></h1>
-                <div class="mt-4 text-center">
-                    <img src="<?= ADDRESS_SITE ?>ressources/images/recipesImages/<?= $recipe['recipeImage'] ?>" alt="<?= $recipe['recipeName'] ?>" width="300" class="img-responsive img-fluid rounded">
-                </div>
+            </div>
+            <div class="col-4"></div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6 mt-4 text-center">
+                <img src="<?= ADDRESS_SITE ?>ressources/images/recipesImages/<?= $recipe['recipeImage'] ?>" alt="<?= $recipe['recipeName'] ?>" class="img-fluid rounded">
+            </div>
+            <div class="col-3"></div>
+        </div>
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4 text-center">
                 <div class="mt-4 text-center">
                     <p><i><?=wordwrap($recipe['description'],60,"\n",true) ?></i></p>
-                </div>
+                </div>  
             </div>
             <div class="col-4"></div>
         </div>
@@ -41,7 +57,7 @@ $recipeSteps = $getRecipeSteps->fetchAll(PDO::FETCH_ASSOC);
                 <u><h3 class="lang-recipe-ingredient-list"></h3></u>
                 <?php
                 echo '
-                <div class="row mt-5" style="padding-left: 8rem; margin-right:5px; ">';
+                <div class="row mt-5" style="padding-left: 8rem; margin-right:10px; ">';
                 foreach ($recipeIngredients as $recipeIngredient) {
                     echo '
                     <div class="col-6 text-center" style="background: #fe9c90; border: 1px solid #333; border-radius: 10px;">
@@ -59,7 +75,7 @@ $recipeSteps = $getRecipeSteps->fetchAll(PDO::FETCH_ASSOC);
                 ?>
             </div>
             <div class="col-4">
-                <h3 class="text-center lang-steps"></h3>
+                <u><h3 class="text-center lang-steps"></h3></u>
                 <?php
                     foreach($recipeSteps as $step) {
                         echo '
@@ -68,7 +84,11 @@ $recipeSteps = $getRecipeSteps->fetchAll(PDO::FETCH_ASSOC);
                     }
                 ?>
             </div>
-            <div class="col-4"></div>
+            <div class="col-4 text-center">
+                <u><h3 class="lang-recipe-author"></h3></u>
+                <img src="<?= ADDRESS_IMG_PROFIL . $authorProfilePicture ?>" alt="<?= $authorInfos['firstname'].' '. $authorInfos['lastname'] ?>" class="img-fluid rounded-circle mt-4" style="width: 150px; height: 150px; border: 1px solid #333;">
+                <h4 class="mt-4"><?= $authorInfos['firstname'].' '.$authorInfos['lastname'] ?></h4>
+            </div>
         </div>
     </main>
 </body>
