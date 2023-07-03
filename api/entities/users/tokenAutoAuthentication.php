435f84eda02e2e7977bec67d1dc14534809fd97d
AriesKira <linharesrodrigues.gil@gmail.com>
2023-07-03 18:28:40 +0200

    Added : token Auth

---
+++ api/entities/users/tokenAutoAuthentication.php
@@ -0,0 +1,36 @@
+<?php
+
+function valid_token(string $token) {
+    global $db;
+
+    $tokenQuery = $db->prepare('SELECT COUNT(*) AS tokenIsValid FROM users WHERE token = :token');
+    $tokenQuery->execute([
+        'token' => $token
+    ]);
+
+    $tokenIsValid = $tokenQuery->fetch();
+
+    if ($tokenIsValid['tokenIsValid'] == 0) {
+        return false;
+    }
+
+    $getUserMailQuery = $db->prepare('SELECT email FROM users WHERE token = :token');
+    $getUserMailQuery->execute([
+        'token' => $token
+    ]);
+
+    $email = $getUserMailQuery->fetch();
+
+    $token = bin2hex(random_bytes(32));
+
+    $updateTokenQuery = $db->prepare('UPDATE users SET token = :token WHERE email = :email');
+    $updateTokenQuery->execute([
+        'token' => $token,
+        'email' => $email
+    ]);
+
+    return [
+        'token' => $token
+    ];
+
+}
---
+++ api/routes/users/connectUserToken.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . "/../../libraries/body.php";
+require_once __DIR__ . "/../../libraries/header.php";
+require_once __DIR__ . "/../../entities/users/tokenAutoAuthentication.php";
+
+header("Content-Type: application/json");
+
+try {
+    $token = getAuthorizationBearerToken();
+
+    if (!$token) {
+        throw new Exception("Provide an Authorization: Bearer token",401);
+    }
+
+    if ($newToken = valid_token($token)) {
+        throw new Exception("Invalid Token",401);
+    }
+
+    http_response_code(200);
+    echo json_encode([
+        "success" => true,
+        "token" => $newToken["token"]
+    ]);
+    die();
+}catch (Exception $e) {
+    echo json_encode([
+        "success" => false,
+        "message" => $e->getMessage()
+    ]);
+    die();
+}
